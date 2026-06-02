3bb3b45cbfb817ee0db9043bb8f1fa9cd41cc551
Muhammad <solomonkinng@gmail.com>
2026-06-02 11:14:37 +0200

    Add constructor

--- BehaviorTree.Core/ANode.cs
+++ BehaviorTree.Core/ANode.cs
@@ -1,8 +1,8 @@
 namespace BehaviorTree.Core
 {
-    public abstract class ANode
+    public abstract class ANode(string pName = "")
     {
-        public string Name = "";
+        public string Name = pName;
         public abstract Result Tick();
     }
 }
