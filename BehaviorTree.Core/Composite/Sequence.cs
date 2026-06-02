c783767bdc60a173b044ccff404f8307a696e21e
Muhammad <solomonkinng@gmail.com>
2026-06-02 10:49:39 +0200

    Add Sequence Node

---
+++ BehaviorTree.Core/Composite/Sequence.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel;
+using BehaviorTree.Core.Interfaces;
+
+namespace BehaviorTree.Core.Composite;
+
+public class Sequence : ANode, IComposite
+{
+    public List<ANode> Children { get; } = [];
+    private int _LastChildrenIndex = 0;
+
+    public Result ProcessChildren()
+    {
+        for (int i = _LastChildrenIndex; i < Children.Count; i++)
+        {
+            Result lCurrentChildResult = Children[i].Tick();
+
+            if (lCurrentChildResult == Result.FAILURE)
+            {
+                _LastChildrenIndex = 0;
+                return Result.FAILURE;
+            }
+
+            else if (lCurrentChildResult == Result.RUNNING)
+            {
+                _LastChildrenIndex = i;
+                return Result.RUNNING;
+            }
+        }
+
+        _LastChildrenIndex = 0;
+        return Result.SUCCESS;
+    }
+
+    public void Add(ANode pNode) => Children.Add(pNode);
+    public override Result Tick()
+    {
+        return ProcessChildren();
+    }
+}
