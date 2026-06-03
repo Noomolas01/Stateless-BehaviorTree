e4f9e2bb558a786852b0d68b3c7dda320abe6963
Muhammad <solomonkinng@gmail.com>
2026-06-03 16:37:02 +0200

    Add FakeAI test

--- BehaviorTree.Demo/Program.cs
+++ BehaviorTree.Demo/Program.cs
@@ -1,5 +1,9 @@
-﻿using BT = BehaviorTree.Core.Tree.BehaviorTree;
+﻿using BehaviorTree.Core.Node.Leaf;
+using BT = BehaviorTree.Core.Tree.BehaviorTree;
 
 BT lBehaviorTree = new BT.Builder()
                    .Sequence()
+                   .Action(new MoveAction())
                    .Build();
+
+Console.WriteLine(lBehaviorTree.Tick());
