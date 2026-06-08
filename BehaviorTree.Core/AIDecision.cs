30ec40307ba8782525cb275999034d8cfd18bbe6
Muhammad <solomonkinng@gmail.com>
2026-06-08 09:53:00 +0200

    Fix: move Attack & MoveDecision to Fake folder

--- BehaviorTree.Core/AIDecision.cs
+++ BehaviorTree.Core/AIDecision.cs
@@ -1,20 +1,4 @@
-using BehaviorTree.Core.Struct;
-
 namespace BehaviorTree.Core;
 
 public interface IAIDecision;
 
-public readonly struct MoveDecision(Vector2 pDestination) : IAIDecision
-{
-    public readonly Vector2 destination = pDestination;
-}
-
-public enum AttackType
-{
-    MELEE,
-    RANGE
-}
-public readonly struct AttackDecision(AttackType pAttackType) : IAIDecision
-{
-    public readonly AttackType attackType = pAttackType;
-}
