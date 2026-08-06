ee897070cb85274870f203eaa5310a9dfcfbc475
Muhammad <solomonkinng@gmail.com>
2026-08-06 15:47:51 +0200

    refactor: WIP classes as internal

--- BehaviorTree.Core/Node/Decorator/Inverter.cs
+++ BehaviorTree.Core/Node/Decorator/Inverter.cs
@@ -1,22 +1,29 @@
-using BehaviorTree.Core;
 using BehaviorTree.Core.Node.Abstract;
-using BehaviorTree.Core.Tree.DataManagement;
-public class Inverter : ADecorator
-{
-    protected override TickResult Decorate(ANode pNode, Blackboard pWorldContext, Blackboard pMemory)
-    {
-        TickResult lChildTickResult = child!.Tick(pWorldContext, pMemory);
+using BehaviorTree.Core.Node.Decorator.Abstract;
+using BehaviorTree.Core.Tree.Blackboard;
+using BehaviorTree.Core.Tree.Results;
 
-        switch (lChildTickResult.status)
+namespace BehaviorTree.Core.Node.Decorator
+{
+    // WIP
+    internal class Inverter : ADecorator
+    {
+ 
+        protected override TickResult Decorate(ANode pNode, Blackboard pWorldContext, Blackboard pMemory)
         {
-            case NodeStatus.SUCCESS:
-                return new TickResult(NodeStatus.FAILURE, lChildTickResult.decision, pMemory);
-            case NodeStatus.RUNNING:
-                return lChildTickResult;
-            case NodeStatus.FAILURE:
-                return new TickResult(NodeStatus.SUCCESS, lChildTickResult.decision, pMemory);
-            default:
-                throw new System.Exception("Decorator didn't return a valid node' status");
+            TickResult lChildTickResult = child!.Tick(pWorldContext, pMemory);
+
+            switch (lChildTickResult.status)
+            {
+                case NodeStatus.SUCCESS:
+                    return new TickResult(NodeStatus.FAILURE, lChildTickResult.decision, pMemory);
+                case NodeStatus.RUNNING:
+                    return lChildTickResult;
+                case NodeStatus.FAILURE:
+                    return new TickResult(NodeStatus.SUCCESS, lChildTickResult.decision, pMemory);
+                default:
+                    throw new System.Exception("Decorator didn't return a valid node' status");
+            }
         }
     }
 }
--- BehaviorTree.Core/Node/Decorator/Repeat.cs
+++ BehaviorTree.Core/Node/Decorator/Repeat.cs
@@ -1,23 +1,34 @@
-using BehaviorTree.Core;
 using BehaviorTree.Core.Node.Abstract;
-using BehaviorTree.Core.Tree.DataManagement;
-public class Repeat : ADecorator
+using BehaviorTree.Core.Node.Decorator.Abstract;
+using BehaviorTree.Core.Tree.Blackboard;
+using BehaviorTree.Core.Tree.Results;
+
+namespace BehaviorTree.Core.Node.Decorator
 {
-    private readonly int _count;
-    public Repeat(int pCount)
+    // WIP
+    internal class Repeat : ADecorator
     {
-        _count = pCount;
-    }
-
-    protected override TickResult Decorate(ANode pNode, Blackboard pWorldContext, Blackboard pMemory)
-    {
-        TickResult lChildTickResult = default;
-
-        for(int i = 0; i < _count; i++)
+        private readonly int _count;
+        private int _current = 0;
+        public Repeat(int pCount)
         {
-            lChildTickResult = child!.Tick(pWorldContext, pMemory);
-        }   
+            _count = pCount;
+        }
 
-        return lChildTickResult;
+        protected override TickResult Decorate(ANode pNode, Blackboard pWorldContext, Blackboard pMemory)
+        {
+            TickResult lChildTickResult;
+
+            lChildTickResult = child!.Tick(pWorldContext, pMemory);
+
+            if (lChildTickResult.status == NodeStatus.RUNNING)
+                return lChildTickResult;
+
+            _current++;
+
+            throw new System.NotImplementedException();
+
+
+        }
     }
 }
