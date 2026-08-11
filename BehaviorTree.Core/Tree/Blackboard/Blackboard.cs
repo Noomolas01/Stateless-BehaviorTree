8846ef3af727c89e848ace951c5754c596b6c24a
Muhammad <solomonkinng@gmail.com>
2026-08-11 22:29:38 +0200

    [ADD] Get & Set blackboard's data w/ string

--- BehaviorTree.Core/Tree/Blackboard/Blackboard.cs
+++ BehaviorTree.Core/Tree/Blackboard/Blackboard.cs
@@ -45,5 +45,40 @@ namespace BehaviorTree.Core.Tree.Blackboard
                 Console.WriteLine($"Key: {pKey.name} doesn't exist.");
             }
         }
+
+        public bool Get<T>(string pKey, out T pValue)
+        {
+            if (_kvp.TryGetValue(pKey, out object value))
+            {
+                if (value is T castValue)
+                {
+                    pValue = castValue;
+                    return true;
+                }
+            }
+
+            Console.WriteLine($"Key: {pKey} doesn't exist.");
+            pValue = default!;
+            return false;
+        }
+
+        public void Set<T>(string pKey, T pValue) where T : notnull
+        {
+            if (!_kvp.ContainsKey(pKey))
+            {
+                Console.WriteLine($"Key: {pKey} added.");
+            }
+
+            _kvp[pKey] = pValue!;
+        }
+
+        public void Remove<T>(string pKey)
+        {
+            if (!_kvp.Remove(pKey))
+            {
+                Console.WriteLine($"Key: {pKey} doesn't exist.");
+            }
+        }
+
     }
 }
