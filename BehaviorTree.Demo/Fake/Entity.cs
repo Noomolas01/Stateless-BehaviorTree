9bb94d7445af1cc94d6a7439042b1cc1c88262ae
Muhammad <solomonkinng@gmail.com>
2026-08-15 01:13:58 +0200

    [WIP] Components have a ref to their owner
    [WIP] Combat Component intializes the memory with their kvp

--- BehaviorTree.Demo/Fake/Entity.cs
+++ BehaviorTree.Demo/Fake/Entity.cs
@@ -4,6 +4,7 @@ using BehaviorTree.Core.Tree;
 using BehaviorTree.Core.Tree.Blackboard;
 using BehaviorTree.Core.Tree.Results;
 using BehaviorTree.Demo.Fake.Decisions;
+using System.Text;
 
 namespace BehaviorTree.Demo.Fake
 {
@@ -12,20 +13,29 @@ namespace BehaviorTree.Demo.Fake
         private readonly AIComponent _aiComponent;
         private readonly MovementComponent _movementComponent;
         private readonly CombatComponent _combatComponent;
+        private readonly Blackboard _memory = new();
 
-        private readonly Blackboard _memory = new Blackboard();
+        public readonly string id;
+        public readonly Core.Tree.BehaviorTree tree;
 
-        public Entity()
+        public Entity(string pid, Core.Tree.BehaviorTree pTree)
         {
-            Core.Tree.BehaviorTree lTree = new Core.Tree.BehaviorTree.Builder()
-                                          .Build();
-
-            _aiComponent = new AIComponent(lTree, 1000, _memory);
+            id = pid;
+            tree = pTree;
+            _memory.Set("DebugOutput", new StringBuilder());
+            _aiComponent = new AIComponent(this,tree, 1 / 60f, _memory);
             _movementComponent = new MovementComponent(_memory);
             _combatComponent = new CombatComponent(_memory);
 
             _aiComponent.sendDecision += _movementComponent.OnDecision;
             _aiComponent.sendDecision += _combatComponent.OnDecision;
+        }
+
+        public void Update(float pDeltaTime)
+        {
+            _aiComponent.Update(pDeltaTime);
+            _movementComponent.Update(pDeltaTime);
+            _combatComponent.Update(pDeltaTime);
         }
     }
     internal abstract class AComponent
@@ -47,6 +57,9 @@ namespace BehaviorTree.Demo.Fake
        private int _count;
         public CombatComponent(Blackboard pMemory) : base(pMemory)
         {
+            // Init
+            pMemory.Set("AttackFinished", false);
+            pMemory.Set("AttackStarted", false);
         }
 
         public override void OnDecision(IAIDecision pAIDecision)
@@ -57,9 +70,10 @@ namespace BehaviorTree.Demo.Fake
             if (_isBusy)
                 return;
 
-            _isBusy = true;
+           // _isBusy = true;
 
             // Here initialize combat logic... 
+            Console.WriteLine("Received a Combat Decision");
         }
 
         public override void Update(float pDeltaTime)
@@ -87,7 +101,9 @@ namespace BehaviorTree.Demo.Fake
 
             _isBusy = true;
 
+            Console.WriteLine("Received a Movement Decision");
             // Here intialize movement logic
+
         }
 
         public override void Update(float pDeltaTime)
@@ -105,11 +121,15 @@ namespace BehaviorTree.Demo.Fake
         public Blackboard memory;
         public Action<IAIDecision>? sendDecision;
 
-        public AIComponent(Core.Tree.BehaviorTree pTree, float pTickDuration, Blackboard pBlackboard)
+        private readonly Entity _owner;
+        
+
+        public AIComponent(Entity pOwner, Core.Tree.BehaviorTree pTree, float pTickDuration, Blackboard pBlackboard)
         {
             _tree = pTree;
             _timeBetweenTick = pTickDuration;
             memory = pBlackboard;
+            _owner = pOwner;
         }
 
         public void Update(float pDeltaTime)
@@ -119,6 +139,7 @@ namespace BehaviorTree.Demo.Fake
             if (_ElapsedTime > _timeBetweenTick)
             {
                 IAIDecision? lDecision = _tree.Tick(null!, memory).decision;
+                Console.WriteLine($"{_owner.id}'s tree ticked");
 
                 if (lDecision != null)
                     sendDecision?.Invoke(lDecision);
