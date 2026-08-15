5c9cc8732f81fc3b8df43e28f3743558937abe91
Muhammad <solomonkinng@gmail.com>
2026-08-15 17:36:29 +0200

    [UPDATE] Entity doesn't initialize ai component

--- BehaviorTree.Demo/Fake/Entity.cs
+++ BehaviorTree.Demo/Fake/Entity.cs
@@ -1,31 +1,25 @@
-﻿
-
-using BehaviorTree.Core.Tree;
-using BehaviorTree.Core.Tree.Blackboard;
-using BehaviorTree.Core.Tree.Results;
-using BehaviorTree.Demo.Fake.Decisions;
+﻿using BehaviorTree.Core.Tree.Blackboard;
+using BehaviorTree.Demo.Fake.Components;
 using System.Text;
 
 namespace BehaviorTree.Demo.Fake
 {
     internal class Entity
     {
-        private readonly AIComponent _aiComponent;
+        public readonly AIComponent _aiComponent;
         private readonly MovementComponent _movementComponent;
         private readonly CombatComponent _combatComponent;
-        private readonly Blackboard _memory = new();
+        public Blackboard Memory { get; private set; } = new();
 
         public readonly string id;
-        public readonly Core.Tree.BehaviorTree tree;
 
-        public Entity(string pid, Core.Tree.BehaviorTree pTree)
+        public Entity(string pid)
         {
             id = pid;
-            tree = pTree;
-            _memory.Set("DebugOutput", new StringBuilder());
-            _aiComponent = new AIComponent(this,tree, 1 / 60f, _memory);
-            _movementComponent = new MovementComponent(_memory);
-            _combatComponent = new CombatComponent(_memory);
+            Memory.Set("DebugOutput", new StringBuilder());
+            _aiComponent = new AIComponent(this);
+            _movementComponent = new MovementComponent(Memory);
+            _combatComponent = new CombatComponent(Memory);
 
             _aiComponent.sendDecision += _movementComponent.OnDecision;
             _aiComponent.sendDecision += _combatComponent.OnDecision;
@@ -38,18 +32,9 @@ namespace BehaviorTree.Demo.Fake
 
         public void Update(float pDeltaTime)
         {
-            _ElapsedTime += pDeltaTime;
-
-            if (_ElapsedTime > _timeBetweenTick)
-            {
-                IAIDecision? lDecision = _tree.Tick(null!, memory).decision;
-                Console.WriteLine($"{_owner.id}'s tree ticked");
-
-                if (lDecision != null)
-                    sendDecision?.Invoke(lDecision);
-
-                _ElapsedTime = 0f;
-            }
+            _movementComponent.Update(pDeltaTime);
+            _combatComponent.Update(pDeltaTime);
+            _aiComponent.Update(pDeltaTime);
         }
     }
 }
