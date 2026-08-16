bc6086333ff76e44ea36c7eabb2b82d96f01804a
Muhammad <solomonkinng@gmail.com>
2026-08-16 02:01:18 +0200

    [ADD] fake attack cooldown managing & attack processing

--- BehaviorTree.Demo/Fake/Components/CombatComponent.cs
+++ BehaviorTree.Demo/Fake/Components/CombatComponent.cs
@@ -1,17 +1,25 @@
 ﻿using BehaviorTree.Core.Tree.Blackboard;
 using BehaviorTree.Core.Tree.Results;
 using BehaviorTree.Demo.Fake.Decisions;
+using System;
 
 namespace BehaviorTree.Demo.Fake.Components
 {
     internal class CombatComponent : AComponent
     {
-        private int _count;
+        private int _frameCount;
+        private int _attackDurationInFrame = 5;
+        private int _attackCooldownInFrame = 10;
+
+        private int _currentCooldown;
+
         public CombatComponent(Blackboard pMemory) : base(pMemory)
         {
             // Init
             pMemory.Set("AttackFinished", false);
             pMemory.Set("AttackStarted", false);
+            memory.Set("IsAttackReady", false);
+
         }
 
         public override void OnDecision(IAIDecision pAIDecision)
@@ -22,7 +30,7 @@ namespace BehaviorTree.Demo.Fake.Components
             if (_isBusy)
                 return;
 
-            // _isBusy = true;
+            _isBusy = true;
 
             // Here initialize combat logic... 
             Console.WriteLine("Received a Combat Decision");
@@ -30,16 +38,31 @@ namespace BehaviorTree.Demo.Fake.Components
 
         public override void Update(float pDeltaTime)
         {
-            // Here handle combat logic...
-            _count++;
+            if (_currentCooldown < _attackCooldownInFrame)
+            {
+                Console.WriteLine("Attack is not ready yet");
+                _currentCooldown++;
+                return;
+            }
 
+            else
+            {
+               memory.Set("IsAttackReady", true);
+
+            }
+            // Here handle combat logic...
+            _frameCount++;
+            Console.WriteLine("Attack is processing...");
 
             // When component has finished its job, it writes in the Memory how it went
-            if (_count >= 5)
+            if (_frameCount >= _attackDurationInFrame)
             {
                 memory.Set("AttackFinished", true);
                 Console.WriteLine("Attack is done");
-                _count = 0;
+                memory.Set("IsAttackReady", false);
+                _frameCount = 0;
+                _currentCooldown = 0;
+                _isBusy = false;
             }
         }
     }
