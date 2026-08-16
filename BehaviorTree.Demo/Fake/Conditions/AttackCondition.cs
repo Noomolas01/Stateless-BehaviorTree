3e0a15fb3212d1a65dd488d7d2881f892f410c12
Muhammad <solomonkinng@gmail.com>
2026-08-16 02:00:34 +0200

    [ADD] Attack condition node

---
+++ BehaviorTree.Demo/Fake/Conditions/AttackCondition.cs
@@ -0,0 +1,28 @@
+﻿using BehaviorTree.Core.Node.Leaf.Abstract;
+using BehaviorTree.Core.Tree.Blackboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorTree.Demo.Fake.Conditions
+{
+    internal class AttackCondition : AConditionNode
+    {
+        public AttackCondition() 
+        {
+            name = typeof(AttackCondition).Name;
+        }
+
+        protected override bool Evaluate(Blackboard pWorldContext, Blackboard pMemory)
+        {
+            if (!pMemory.Get("IsAttackReady", out bool lIsReady))
+            {
+                throw new Exception("Key IsAttackReady is not in memory");
+            }
+
+            return lIsReady;
+        }
+    }
+}
