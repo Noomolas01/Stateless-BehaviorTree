4d15366b5d3bf49296da494d07b1a8d209a308a4
Muhammad <solomonkinng@gmail.com>
2026-08-18 18:20:53 +0200

    [ADD] Do Movement action node

---
+++ BehaviorTree.Demo/Fake/Actions/DoMovement.cs
@@ -0,0 +1,37 @@
+﻿using BehaviorTree.Core.Node.Leaf.Abstract;
+using BehaviorTree.Core.Tree.Blackboard;
+using BehaviorTree.Core.Tree.Results;
+using BehaviorTree.Demo.Fake.Decisions;
+
+
+namespace BehaviorTree.Demo.Fake.Actions
+{
+    internal class DoMovement : AActionNode
+    {
+        public DoMovement()
+        {
+            name = GetType().Name;
+        }
+
+        protected override TickResult Do(Blackboard pWorldContext, Blackboard pMemory)
+        {
+            if (!pMemory.TryGet("MovementStarted", out bool hasStarted))
+                throw new Exception("Key MovementStarted is not in memory");
+
+            if (!pMemory.TryGet("MovementFinished", out bool hasFinished))
+                throw new Exception("Key MovementStarted is not in memory");
+
+
+            if (hasStarted && hasFinished)
+            {
+                pMemory.Set("MovemementStarted", false);
+                pMemory.Set("MovemementFinished", false);
+                return new TickResult(Core.NodeStatus.SUCCESS, null, pMemory);
+            }
+
+            pMemory.Set("MovementStarted", true);
+            return new TickResult(Core.NodeStatus.RUNNING, new CombatDecision(), pMemory);
+
+        }
+    }
+}
