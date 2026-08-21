b6750367ee2fc7afff7d48f1b6d2dcc6bb619817
Muhammad <solomonkinng@gmail.com>
2026-08-21 14:53:16 +0200

    [REMOVE] unused decision's field

--- BehaviorTree.Demo/Fake/Decisions/Decisions.cs
+++ BehaviorTree.Demo/Fake/Decisions/Decisions.cs
@@ -6,11 +6,11 @@ namespace BehaviorTree.Demo.Fake.Decisions
 {
     internal struct MovementDecision : IAIDecision
     {
-        public Vector3 destination;
+        
     }
 
     internal struct CombatDecision : IAIDecision
     {
-        public string type;
+        
     }
 }
