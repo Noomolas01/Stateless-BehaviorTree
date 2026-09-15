ed93e36776507b9a64b602faf2bb190cd2f7c823
Muhammad <solomonkinng@gmail.com>
2026-09-15 22:39:19 +0200

    [changes] move function by name in Blackboard script

--- StatelessBehaviorTree.Core/Tree/Blackboard/Blackboard.cs
+++ StatelessBehaviorTree.Core/Tree/Blackboard/Blackboard.cs
@@ -15,6 +15,33 @@ namespace StatelessBehaviorTree.Core.Tree.Blackboard
     {
         private readonly Dictionary<string, object> _objectsByID = new Dictionary<string, object>();
 
+
+        /// <summary>
+        /// Provides a non-blocking alternative to <see cref="Get{T}(string)"/>
+        /// </summary>
+        /// <remarks>
+        /// This method is <b>type-unsafe</b>. Only use this if you want to iterate fast.
+        /// It is not recommanded to use this in production since nothing guarantee <typeparamref name="T"/> matches the value associated with the <paramref name="pKey"/> in the <see cref="Blackboard"/>. <br/>
+        /// Use this if you want to add a fallback when a key is missing in the <see cref="Blackboard"/>. <br/>
+        /// If you want to throw an exception if the key is missing, use <see cref="Get{T}(BBKey{T})"/> instead.
+        /// </remarks>
+        /// <typeparam name="T">Value's type</typeparam>
+        /// <returns> returns <c>true</c> if tke key is present in the blackboard, otherwise returns <c>false</c> </returns>
+        public bool TryGet<T>(string pKey, out T pValue)
+        {
+            if (_objectsByID.TryGetValue(pKey, out object value))
+            {
+                if (value is T castValue)
+                {
+                    pValue = castValue;
+                    return true;
+                }
+            }
+
+            pValue = default!;
+            throw new KeyNotFoundException($"Key: {pKey} doesn't exist.");
+        }
+
         /// <summary>
         /// Provides a not blocking alternative to <see cref="Get{T}(BBKey{T})"/>
         /// </summary>
@@ -38,6 +65,7 @@ namespace StatelessBehaviorTree.Core.Tree.Blackboard
             pValue = default!;
             return false;
         }
+
         /// <summary>
         /// Get a value in the blackboard with a blackboard key. 
         /// </summary>
@@ -83,66 +111,23 @@ namespace StatelessBehaviorTree.Core.Tree.Blackboard
             throw new KeyNotFoundException($"Key: {pKey} doesn't exist.");
         }
 
-        public void Set<T>(BBKey<T> pKey, T pValue) where T :notnull
-        {
-            if (!_objectsByID.ContainsKey(pKey.name))
-            {
-                Console.WriteLine($"Key: {pKey.name} added.");
-            }
 
-            _objectsByID[pKey.name] = pValue!;
+        public bool Remove<T>(BBKey<T> pKey)
+        {
+            return _objectsByID.Remove(pKey.name);
         }
-
-        public void Remove<T>(BBKey<T> pKey)
+        public bool Remove<T>(string pKey)
         {
-            if (!_objectsByID.Remove(pKey.name))
-            {
-                throw new KeyNotFoundException($"Key: {pKey.name} doesn't exist.");
-            }
-        }
-
-        /// <summary>
-        /// Provides a non-blocking alternative to <see cref="Get{T}(string)"/>
-        /// </summary>
-        /// <remarks>
-        /// This method is <b>type-unsafe</b>. Only use this if you want to iterate fast.
-        /// It is not recommanded to use this in production since nothing guarantee <typeparamref name="T"/> matches the value associated with the <paramref name="pKey"/> in the <see cref="Blackboard"/>. <br/>
-        /// Use this if you want to add a fallback when a key is missing in the <see cref="Blackboard"/>. <br/>
-        /// If you want to throw an exception if the key is missing, use <see cref="Get{T}(BBKey{T})"/> instead.
-        /// </remarks>
-        /// <typeparam name="T">Value's type</typeparam>
-        /// <returns> returns <c>true</c> if tke key is present in the blackboard, otherwise returns <c>false</c> </returns>
-        public bool TryGet<T>(string pKey, out T pValue)
-        {
-            if (_objectsByID.TryGetValue(pKey, out object value))
-            {
-                if (value is T castValue)
-                {
-                    pValue = castValue;
-                    return true;
-                }
-            }
-
-            pValue = default!;
-            throw new KeyNotFoundException($"Key: {pKey} doesn't exist.");
+            return _objectsByID.Remove(pKey);
         }
 
         public void Set<T>(string pKey, T pValue) where T : notnull
         {
-            if (!_objectsByID.ContainsKey(pKey))
-            {
-                Console.WriteLine($"Key: {pKey} added.");
-            }
-
             _objectsByID[pKey] = pValue!;
         }
-
-        public void Remove<T>(string pKey)
+        public void Set<T>(BBKey<T> pKey, T pValue) where T : notnull
         {
-            if (!_objectsByID.Remove(pKey))
-            {
-                throw new KeyNotFoundException($"Key: {pKey} doesn't exist.");
-            }
+            _objectsByID[pKey.name] = pValue!;
         }
 
         public override string ToString()
@@ -151,7 +136,7 @@ namespace StatelessBehaviorTree.Core.Tree.Blackboard
 
             foreach (var kvp in _objectsByID)
             {
-                lSb.Append(kvp.Key).Append(" : ").Append(kvp.Value).AppendLine() ;
+                lSb.Append(kvp.Key).Append(" : ").Append(kvp.Value).AppendLine();
             }
 
             return lSb.ToString();
