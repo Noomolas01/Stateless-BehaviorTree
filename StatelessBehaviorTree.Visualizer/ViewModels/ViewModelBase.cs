410f7d80bb8a3c58ccfea9fd9fa31e2da90756e0
Muhammad <solomonkinng@gmail.com>
2026-09-16 12:50:03 +0200

    [add] Avalonia setup & Avalonia Template

---
+++ StatelessBehaviorTree.Visualizer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace StatelessBehaviorTree.Visualizer.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ StatelessBehaviorTree.Visualizer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace StatelessBehaviorTree.Visualizer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
